f6d0d2e1771e956c21cf307e085fa5dde1316f34
soncrayon <arnold.curtis@gmail.com>
2021-06-24 22:12:23 -0400

    20210624

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const largestSubarraySum = (arr) => {
-    let largestSum = 0
-    for (let i=0; i<arr.length; i++){
-        let subarraySum = arr.slice(0,(i+1)).reduce((acc, n) => {acc + n})
-        subarraySum > largestSum ? largestSum = subarraySum : largestSum
+    let largestSum = arr.reduce((acc, n) => {return acc + n})
+    let negTest = [...arr]
+    negTest.map((n) => {return n < 0})
+    console.log(arr)
+    console.log(negTest)
+    if (negTest.length > 0){
+        // if (negTest.length === arr.length){
+        //     largestSum = Math.max(...arr)
+        // }
+        for (let i=0; i<(arr.length-1); i++){
+    
+    
+        }
     }
     return largestSum 
 }
